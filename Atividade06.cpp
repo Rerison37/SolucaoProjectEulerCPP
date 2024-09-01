ee2d0b15afc48f7c762e0bf0a568bd90059241a1
Rerison <rerisonsilvaraujo@gmail.com>
2024-09-01 18:27:30 -0300

    Atividade06

---
+++ Atividade06.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    int sum_squares = 0;
+    int square_sum = 0;
+
+    for (int i = 1; i <= 100; i++)
+    {
+        sum_squares += i * i;
+    }
+
+    for (int j = 1; j <= 100; j++)
+    {
+        square_sum += j;
+    }
+
+    square_sum = square_sum * square_sum;
+    int diference = square_sum - sum_squares;
+    
+    cout << "The diference is: " << diference << endl;
+    
+}
